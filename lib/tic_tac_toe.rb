5270623f1b0f16746c0def45d69ba98f4945cfcf
darren mcgregor <darren.mcgregor@avanzac.co.uk>
2018-08-22 15:25:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,8 +11,8 @@ class TicTacToe
     [2,4,6] # diagonal b
   ]
 
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
+  def initialize
+    @board = Array.new(9, " ")
   end
 end
 
